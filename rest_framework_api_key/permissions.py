c6b7399d6a07f814ecbc595f0f015d04dd7c63a6
Adam Hill <adamghill@yahoo.com>
2016-12-06 22:04:52 -0500

    Cache the API key lookup.

--- rest_framework_api_key/permissions.py
+++ rest_framework_api_key/permissions.py
@@ -1,3 +1,5 @@
+from django.core.cache import cache
+
 from rest_framework import permissions
 from rest_framework_api_key.models import APIKey
 
@@ -11,4 +13,16 @@ class HasAPIAccess(permissions.BasePermission):
         if not api_key:
             api_key = request.GET.get('apikey', '')
 
-        return APIKey.objects.filter(key=api_key).exists()
+        if api_key:
+            cache_key = 'rest_framework_api_key:{}'.format(api_key)
+            api_key_does_exist = cache.get(cache_key, None)
+
+            if api_key_does_exist is None:
+                api_key_does_exist = APIKey.objects.filter(key=api_key).exists()
+
+                cache_timeout = 60 * 10
+                cache.set(cache_key, api_key_does_exist, cache_timeout)
+
+            return api_key_does_exist
+
+        return False
